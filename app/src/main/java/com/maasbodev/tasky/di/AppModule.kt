b62631321ba35b48c0780d84618b751d52a26b19
Marçal <marcal.asbo@gmail.com>
2024-07-04 22:33:01 +0200

    splash screen and check auth - pr comments

---
+++ app/src/main/java/com/maasbodev/tasky/di/AppModule.kt
@@ -0,0 +1,20 @@
+package com.maasbodev.tasky.di
+
+import android.content.Context
+import android.content.SharedPreferences
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+	@Singleton
+	@Provides
+	fun provideSharedPreferences(@ApplicationContext context: Context): SharedPreferences {
+		return context.getSharedPreferences("tasky_prefs", Context.MODE_PRIVATE)
+	}
+}
